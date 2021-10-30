54b785cf39af03070dce233829161380027af4a3
Joaquin <omglocura@gmail.com>
2021-10-30 16:04:33 -0300

    Agrego imagenes H y rombos

--- Imagenes/imagenes rombo/write_photo.py
+++ Imagenes/imagenes rombo/write_photo.py
@@ -11,6 +11,6 @@ camera.enable(timeStep)
 while robot.step(timeStep) != -1:
     img = camera.getImage()
     img = np.array(np.frombuffer(img, np.uint8).reshape((camera.getHeight(), camera.getWidth(), 4)))
-    cv2.imwrite("C:/Users/JOAKOL/Desktop/robotica/Siempre Repositorio/Sabado-IITA-robotica/Imagenes/imagenes rombo/rombo_.png" , img)
+    cv2.imwrite("C:/Users/JOAKOL/Desktop/robotica/Siempre Repositorio/Sabado-IITA-robotica/Imagenes/imagenes_H/H_.png" , img)
     cv2.imshow("Image", img)
     cv2.waitKey(1)
