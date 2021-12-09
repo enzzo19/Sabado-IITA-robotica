6d489ad4af4a779f79fc1f1c30c43babafeec1b4
Juarez Enzo <49879701+enzzo19@users.noreply.github.com>
2021-12-09 18:05:51 -0300

    change file

--- controladores/Oro_Salteño_supremo.py
+++ controladores/Oro_Salteño_supremo.py
@@ -104,30 +104,6 @@ def rotar(angulo):
     angulo_actual = 0
     return True
 
-def rotar_b(angulo):
-    global angulo_actual
-    tiempo_anterior = 0
-    #  iniciar_rotacion
-    girar(0.5)
-    # Mientras no llego al angulo solicitado sigo girando
-    while (abs(angulo - angulo_actual) > 1):
-        tiempo_actual = robot.getTime()
-        # print("Inicio rotacion angulo", angulo, "Angulo actual:",angulo_actual)
-        tiempo_transcurrido = tiempo_actual - tiempo_anterior  # tiempo que paso en cada timestep
-        radsIntimestep = abs(gyro.getValues()[1]) * tiempo_transcurrido   # rad/seg * mseg * 1000
-        degsIntimestep = radsIntimestep * 180 / math.pi
-        # print("rads: " + str(radsIntimestep) + " | degs: " + str(degsIntimestep))
-        angulo_actual += degsIntimestep
-        # Si se pasa de 360 grados se ajusta la rotacion empezando desde 0 grados
-        angulo_actual = angulo_actual % 360
-        # Si es mas bajo que 0 grados, le resta ese valor a 360
-        if angulo_actual < 0:
-            angulo_actual += 360
-        tiempo_anterior = tiempo_actual
-        robot.step(timeStep)
-    # angulo_actual = 0
-    return True
-
 def classifyVictim(img):
     finalLetter = ' '
     # cv.imshow("imagen normal", img)
